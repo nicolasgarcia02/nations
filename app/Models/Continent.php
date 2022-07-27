d6be942b218e4522e6c959da6173a286d3e4fda6
nicolasgarcia02 <ngarcia583@misena.edu.co>
2022-07-27 17:45:38 -0500

    Proyecto iniciado

---
+++ app/Models/Continent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Continent extends Model
+{
+    protected $table = "continents";
+    protected $primaryKey = "continent_id";
+    public $timestamps = false;
+    use HasFactory;
+}
